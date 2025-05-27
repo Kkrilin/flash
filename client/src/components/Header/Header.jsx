4cad1ca927b1ce71401d08f12fd8480f46f484a0
kundan singh patel <kundansingh066@gmail.com>
2025-05-27 14:03:49 +0530

    small change done

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -4,9 +4,9 @@ export default function Header() {
   return (
     <div className="px-20 py-3 h-14 border-b-neutral-700 border-b">
       <header className="flex items-center gap-40 w-1/2 ">
-        <h1>ActNow</h1>
-        <ul className="flex gap-20 capitalize">
-          <li>find</li>
+        <h1 className="text-2xl">ActNow</h1>
+        <ul className="flex gap-20 capitalize items-center">
+          <li>findActivity</li>
           <li>search</li>
           <li>subscription</li>
           <li>contact us</li>
